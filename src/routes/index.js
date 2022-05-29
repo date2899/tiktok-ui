983628e70c4aaac0779080560b94fb3dd567713b
date2899 <date2899@gmail.com>
2022-05-29 15:04:04 +0700

    bai62: cua hinh routes/layout

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { HeaderOnly } from '~/components/Layout';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+// public Route
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
