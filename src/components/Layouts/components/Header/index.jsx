fa20cb737e6405d5929e8855b07eadb4bc7418c8
date2899 <date2899@gmail.com>
2022-06-01 16:59:32 +0700

    bai64 : xay dung ui phan header

--- src/components/Layouts/components/Header/index.jsx
+++ src/components/Layouts/components/Header/index.jsx
@@ -1,13 +1,33 @@
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
 import React from 'react';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="logo tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 };
