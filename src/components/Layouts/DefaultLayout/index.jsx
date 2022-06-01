5fdff002b25f7f4d6436b69fdbf1e4ec19887560
date2899 <date2899@gmail.com>
2022-06-01 11:11:30 +0700

    bai 63: dung khung layout mac dinh

---
+++ src/components/Layouts/DefaultLayout/index.jsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames/bind';
+import Header from '~/components/Layouts/components/Header';
+import styles from './DefaultLayout.module.scss';
+import Sidebar from './Sidebar';
+
+import React from 'react';
+const cx = classNames.bind(styles);
+
+const DefaultLayout = ({ children }) => {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>children</div>
+            </div>
+        </div>
+    );
+};
+
+export default DefaultLayout;
